6a08ff6882b30e526afb29319d2b500c5f5e3027
Andrei Tatar <andrei@unikraft.io>
2023-07-18 23:53:06 +0200

    Port upstream Shapely 2.0.1
    Files under `generated/` are created by the upstream build process from
    templates; we include these files verbatim for the unikraft build.
    Any potential patches should be clearly marked to ease future updates.
    Files under `importfix/` are correctly namespaced wrapper modules for
    any binary modules that we compile and link into unikraft, and must be
    copied over to the python rootfs.
    Selecting this library will add its files to the python rootfs build.
    
    Signed-off-by: Andrei Tatar <andrei@unikraft.io>

---
+++ importfix/shapely/_geometry_helpers.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from shapely__geometry_helpers import *
+from shapely__geometry_helpers import (
+    __pyx_unpickle_Enum,
+    _geom_factory,
+    __test__,
+    _check_out_array,
+)
---
+++ importfix/shapely/_geos.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+
+from shapely__geos import *
+from shapely__geos import __test__
